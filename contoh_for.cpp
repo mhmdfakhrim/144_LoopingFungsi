2ed1f5015b126c297cc42e470ab3d417e5ec507b
mhmdfakhrim <muhfakhri4@gmail.com>
2024-03-18 14:27:54 +0700

    looping perulangan for

---
+++ contoh_for.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int i;
+  int arr[5];
+
+  for (i = 0; i<5; i++)
+  {
+    cout << i << " : " << "Fahri" << endl;
+
+  }
+
+  for (i = 0; i < 5; i++)
+  {
+    cout << "Masukkan Nama = ";
+    cin >> arr[i];
+
+
+  } 
+
+  for (i = 0; i < 5; i++)
+  {
+    cout << "Nama ke- " << i << " : " << arr[i] << endl; 
+  } 
+}
